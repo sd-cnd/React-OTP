bd6b0dbe982d522f3ccc0894273f19de8fe2a33d
sd-cnd <sumitdey.cnd@gmail.com>
2024-07-07 12:54:39 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import PhoneOtpLogin from './components/phone-login'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Login with phone</h1>
+      <PhoneOtpLogin />
+    </div>
+  )
+}
+
+export default App
